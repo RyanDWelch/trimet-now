5d1efbac6c911af31dfc7cf1788996c7423c4b76
Ryan Welch <rdw1809@gmail.com>
2021-01-14 15:30:18 -0800

    ran yarn upgrade

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { mapStyles } from './mapstyles';
 // import { mapStyles } from './mapstyles-bw.js';
 import "./App.css";
 
-// import { sampledata } from './sampledata';
+import { sampledata } from './sampledata';
 
 const API_URL = "https://developer.trimet.org/ws/v2/vehicles/appID/"+process.env.REACT_APP_TRIMET_API_KEY;
 const defaultCenter = [45.519526,-122.677040];
@@ -28,31 +28,31 @@ export class App extends React.Component {
   }
  
   componentDidMount() {
-    let totalRequests = 0;
-    try {
-      setInterval(async () => {
-        if (totalRequests < 10) {
-          const res = await fetch(API_URL);
-          const json = await res.json();
-          console.log(json);
-          // const filteredVehicles = json.resultSet.vehicle.filter(vehicle => vehicle.type === 'rail');
-          const vehicles = json.resultSet.vehicle;
+    // let totalRequests = 0;
+    // try {
+    //   setInterval(async () => {
+    //     if (totalRequests < 10) {
+    //       const res = await fetch(API_URL);
+    //       const json = await res.json();
+    //       console.log(json);
+    //       // const filteredVehicles = json.resultSet.vehicle.filter(vehicle => vehicle.type === 'rail');
+    //       const vehicles = json.resultSet.vehicle;
 
-          this.setState({
-            isLoaded: true,
-            vehicles: vehicles
-          })
-          totalRequests++;
-        }
-      }, 10000);
-    } catch(e) {
-      console.error(e);
-    }
+    //       this.setState({
+    //         isLoaded: true,
+    //         vehicles: vehicles
+    //       })
+    //       totalRequests++;
+    //     }
+    //   }, 10000);
+    // } catch(e) {
+    //   console.error(e);
+    // }
 
-    // this.setState({
-    //   isLoaded: true,
-    //   vehicles: sampledata.resultSet.vehicle
-    // })
+    this.setState({
+      isLoaded: true,
+      vehicles: sampledata.resultSet.vehicle
+    })
   }
 
   handleClick = (vehicleID) => {
